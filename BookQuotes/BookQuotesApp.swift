9852404c571ccb17c2b22382c9b75b29542750b8
Matthew Dembinski <matthewdembinski@gmail.com>
2024-03-05 11:20:58 -0700

    Initial Commit

---
+++ BookQuotes/BookQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookQuotesApp.swift
+//  BookQuotes
+//
+//  Created by Matthew Dembinski on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct BookQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
