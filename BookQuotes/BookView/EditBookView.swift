c3c276c7a5e198e071e14446aeba22073a5a51f2
Matthew Dembinski <matthewdembinski@gmail.com>
2024-03-08 11:57:09 -0700

    ADD: persistent, in-memory previews

--- BookQuotes/BookView/EditBookView.swift
+++ BookQuotes/BookView/EditBookView.swift
@@ -142,8 +142,10 @@ struct EditBookView: View {
     }
 }
 
-//#Preview {
-//    NavigationStack {
-//        EditBookView()
-//    }
-//}
+#Preview {
+    let preview = Preview(Book.self)
+    return NavigationStack {
+        EditBookView(book: Book.sampleBooks[4])
+            .modelContainer(preview.container)
+    }
+}
